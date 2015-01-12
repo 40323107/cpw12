359b3f0fd0271d8d5344b4ec7d13fd0fe6056f79
40323107 <40323107@gm.nfu.edu.tw>
2015-01-12 19:39:39 +0800

    40323107-cpw12

---
+++ wsgi.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import os
+
+def application(environ, start_response):
+
+    ctype = 'text/plain'
+    if environ['PATH_INFO'] == '/health':
+        response_body = "1"
+    elif environ['PATH_INFO'] == '/env':
+        response_body = ['%s: %s' % (key, value)
+                    for key, value in sorted(environ.items())]
+        response_body = '\n'.join(response_body)
+    else:
+        ctype = 'text/html'
+        response_body = '''def multiplication_table():
+    start = 1
+    end = 10
+    outstring = ""
+    for i in range(start, end, 1):
+        for j in range (start, end, 1):
+            outstring += str(i) + "x" + str(j) + "=" + str(i*j) + "&nbsp;"
+        outstring += "<br />"
+    return outstring
+ 
+print(multiplication_table())'''
+
+    status = '200 OK'
+    response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
+    #
+    start_response(status, response_headers)
+    return [response_body.encode('utf-8') ]
+
+#
+# Below for testing only
+#
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    httpd = make_server('localhost', 8051, application)
+    # Wait for a single request, serve it and quit.
+    httpd.handle_request()
